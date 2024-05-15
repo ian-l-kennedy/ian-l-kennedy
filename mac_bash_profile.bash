7ce632293a7c89857055511a85ec65df9a279fb2
Ian Lindsay Kennedy <76818228+ian-l-kennedy@users.noreply.github.com>
2024-05-15 11:54:30 -0400

    Create mac_bash_profile.bash

---
+++ mac_bash_profile.bash
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+export BASH_SILENCE_DEPRECATION_WARNING=1
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+if [[ -r "/opt/homebrew/etc/profile.d/bash_completion.sh" ]] ; then
+    . "/opt/homebrew/etc/profile.d/bash_completion.sh"
+fi
+
+if [ -f $(brew --prefix)/etc/bash_completion ]; then
+    . $(brew --prefix)/etc/bash_completion
+fi
+
+export PATH=$PATH:/Users/iankennedy/Library/Python/3.9/bin
+
+# Enable the subsequent settings only in interactive sessions
+case $- in
+  *i*) ;;
+    *) return;;
+esac
+
+export OSH='/Users/iankennedy/.oh-my-bash'
+
+OSH_THEME="90210"
+
+completions=()
+aliases=()
+plugins=()
+
+source "$OSH"/oh-my-bash.sh
